ebe23d263391519042ab79e7f2b14b3d3d964168
Moritz Moeller <mm@mxs.de>
2016-11-16 15:36:46 +0100

    ..

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name        = 'nanoca',
     packages    = ['.'],
     scripts     = ['nanoca'],
-    version     = '0.6',
+    version     = '0.7',
     description = 'small library and cli tool for managing a certificate authority',
     author      = 'Moritz Möller',
     author_email= 'mm@mxs.de',
